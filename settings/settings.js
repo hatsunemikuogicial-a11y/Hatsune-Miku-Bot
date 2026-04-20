dd6d8030fcdd19b4d67f7bc1d3bd66b60fa259cb
MikuBot <hatsunemikuogicial@gmail.com>
2026-04-20 17:23:35 -0300

    Add settings configuration for bot

---
+++ settings/settings.js
@@ -0,0 +1,7 @@
+{
+  "Bot": "𝑯ᴀᴛꜱᴜɴᴇ ᴍɪᴋᴜ",
+  "creador": "Yoel",
+  "JpgBot": "https://i.postimg.cc/vH5SHVW5/20260316-151443.jpg",
+  "owner": "5492281367797@s.whatsapp.net",
+  "NAUFRA_KEY": "keygratis11"
+}
